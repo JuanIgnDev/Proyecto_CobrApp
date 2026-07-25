574900a0368a3271b4bcf293e55db3a8d25f2247
JuaniEstanguet <juaniestanguet571@gmail.com>
2026-07-24 22:19:59 -0300

    Agregue la posibilidad de configurar el sistema para que en X cantidad de tiempo, se cierre por inactividad

--- configuracion.go
+++ configuracion.go
@@ -7,30 +7,42 @@ import (
 type Configuracion struct {
 	DiasAlerta int
 	MensajeWP  string
+	Minutos_inactividad int
 }
 
 // ObtenerConfiguracion lee los ajustes de la base de datos
 func ObtenerConfiguracion(db *sql.DB) Configuracion {
 	var c Configuracion
+
 	// Buscamos siempre la fila con id = 1
-	err := db.QueryRow(`SELECT dias_alerta, mensaje_wp FROM configuracion WHERE id = 1`).Scan(&c.DiasAlerta, &c.MensajeWP)
-	
+	err := db.QueryRow(`
+		SELECT dias_alerta, mensaje_wp, minutos_inactividad
+		FROM configuracion
+		WHERE id = 1
+	`).Scan(
+		&c.DiasAlerta,
+		&c.MensajeWP,
+		&c.Minutos_inactividad,
+	)
+
 	if err != nil {
 		// Si por algún motivo falla, devolvemos valores por defecto seguros
 		return Configuracion{
-			DiasAlerta: 45, 
-			MensajeWP: "Hola {nombre}, te escribo para recordarte que tenés un saldo pendiente de ${saldo}.",
+			DiasAlerta:          45,
+			MensajeWP:           "Hola {nombre}, te escribo para recordarte que tenés un saldo pendiente de ${saldo}.",
+			Minutos_inactividad: 30,
 		}
 	}
+
 	return c
 }
 
 // GuardarConfiguracion actualiza la fila 1 con los nuevos datos
-func GuardarConfiguracion(db *sql.DB, dias int, mensaje string) error {
+func GuardarConfiguracion(db *sql.DB, dias int, mensaje string, minutos_inactividad int) error {
 	_, err := db.Exec(`
 		UPDATE configuracion 
-		SET dias_alerta = ?, mensaje_wp = ? 
+		SET dias_alerta = ?, mensaje_wp = ?, minutos_inactividad = ?
 		WHERE id = 1
-	`, dias, mensaje)
+	`, dias, mensaje, minutos_inactividad)
 	return err
 }
--- data/schema.sql
+++ data/schema.sql
@@ -74,12 +74,13 @@ SELECT
       - COALESCE((SELECT SUM(monto) FROM cobro WHERE cobro.cliente_id = c.id), 0) AS saldo_pendiente
 FROM cliente c;
 
--- Agrego esto juani fijate
+
 CREATE TABLE IF NOT EXISTS configuracion (
     id INTEGER PRIMARY KEY CHECK (id = 1),
     dias_alerta INTEGER NOT NULL,
-    mensaje_wp TEXT NOT NULL
+    mensaje_wp TEXT NOT NULL,
+    minutos_inactividad INTEGER CHECK (minutos_inactividad >= 0)
 );
 
-INSERT OR IGNORE INTO configuracion (id,dias_alerta,mensaje_wp)
-VALUES (1, 45, 'Hola {nombre}, te escribo para recordarte que tenés un saldo pendiente de ${saldo}. Avisame cuando puedas regularizarlo. ¡Saludos!');
+INSERT OR IGNORE INTO configuracion (id,dias_alerta,mensaje_wp, minutos_inactividad)
+VALUES (1, 45, 'Hola {nombre}, te escribo para recordarte que tenés un saldo pendiente de ${saldo}. Avisame cuando puedas regularizarlo. ¡Saludos!',30);
